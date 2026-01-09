4cf73b4cff3f2a52b9d131e1e7ea6d5826080ed2
ncatt <anntuongg@gmail.com>
2026-01-09 20:14:55 +0700

    Update ftwsl.sh

--- ftwsl.sh
+++ ftwsl.sh
@@ -1,121 +1,106 @@
 #!/bin/bash
 
-# Changelog (fedora compatibility was mostly maintained throughout 2022-2025, with occasional minor updates):
-# 2020-25-10: Inital release
-# 2022-19-5: Broke code into 2 scripts, one for laptop and one for desktop. 
-# 2025-22-5: Refactored code to remove redundant calls and improve readability. Removed deltaRPM support as it was removed in Fedora 42.
+. common.sh
+
+if [ "$EUID" -ne 0 ]; then
+  echo "please run as root (use sudo or switch to root user)."
+  exit 1
+fi
 
 KERNEL_VERSION=$(uname -r)
 
-# WARNING: MUST ONLY BE USED ON FRESH FEDORA INSTALL WITH INTERNET ACCESS, AS ROOT!!! Run at your own risk! This script enables special tweaks to make fedora more usable. These tweaks include: DNF speed up, delta mirrors, installs RPM Fusion (free and non-free), installs gnome-tweaks, fedy, steam, vlc, support for various multimedia codecs and compression support, snap, better_fonts, wine-devel, fish shell, audacity, and chromium, better SSD support. It is broken up in the way it is for easy tweaking and fixing for future fedora versions. Made by Alex jenkins, follow me on github at https://github.com/rockenman1234 
-
+clear
 echo '
-
-
-
+  / _| |___      _____| |
+ | |_| __\ \ /\ / / __| |
+ |  _| |_ \ V  V /\__ \ |
+ |_|  \__| \_/\_/ |___/_|
 '
-echo '/ _|  ___  __| | ___  _ __ __ _  | |___      _____  __ _| | _____'
-echo '| |_ / _ \/ _\ |/ _ \| |__/ _\ | | __\ \ /\ / / _ \/ _\ | |/ / __|'
-echo '|  _|  __/ (_| | (_) | | | (_| | | |_ \ V  V /  __/ (_| |   <\__ \'
-echo '|_|  \___|\__,_|\___/|_|  \__,_|  \__| \_/\_/ \___|\__,_|_|\_\___/'
-echo 'WARNING: READ THE FOLLOWING VERY CAREFULLY! MUST ONLY BE USED ON FRESH FEDORA INSTALL WITH INTERNET ACCESS, AS ROOT!!! Run at your own risk! The process could take aslong as half an hour depending on your internet connection, but should only take 3-5 minutes with good internet connection. I cannot be held responsible for a bricked install, you have been warned!'
+echo "WARNING: MUST BE USED ON A FRESH FEDORA INSTALL WITH INTERNET ACCESS!"
+echo "This script modifies system configurations. Run at your own risk."
+echo ""
 
-echo '
+if [[ -f /data/data/com.termux/files/usr/bin/termux-setup-storage ]]; then
+    warn "Termux environment detected!"
+    warn "Termux not supported!"
+fi
 
+if [[ $OSTYPE == 'darwin'* ]]; then
+    warn "macOS environment detected!"
+    warn "macOS not supported!"
+fi
 
+if [ ! -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
+    err "WSL enviroment not found! Please use standard Fedora Tweaks!"
+    exit 1
+fi
 
-'
+echo "Accepting the terms above, do you still wish to run the script?"
+echo "You will be asked Yes/No for each individual feature."
+read -p "Type 'yes' to continue, or anything else to exit: " yn
+case $yn in
+    [Yy]* ) ;;
+    * ) exit;;
+esac
 
-echo 'This is fully-autonomous script, please close and save all work before running this script!'
-
-echo '
-
-
-
-'
-
-echo "Accepting the terms above, do you still wish to run? Type 1 or 2."
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) break;;
-        No ) exit;;
-    esac
-done
-
-echo 'This script enables special tweaks to make fedora more usable. These tweaks include: DNF speed up, installs RPM Fusion (free and non-free), installs gnome-tweaks, fedy, steam, vlc, support for various multimedia codecs and compression support, snap, and better_fonts'
-
-# These following lines speed up dnf
-sudo echo 'fastestmirror=true' >> /etc/dnf/dnf.conf
-sudo echo 'max_parallel_downloads=10' >> /etc/dnf/dnf.conf
-
-# Update system and install necessary packages
-sudo dnf update -y
-sudo dnf upgrade --refresh -y
-sudo dnf check-update -y
-sudo dnf install \
-    https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-    https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
-    gnome-tweak-tool \
-    fedy \
-    steam \
-    vlc \
-    rpmfusion-free-release-tainted \
-    libdvdcss \
-    rpmfusion-nonfree-release-tainted \
-    \*-firmware \
-    p7zip p7zip-plugins \
-    cabextract \
-    snapd \
-    fontconfig-enhanced-defaults fontconfig-font-replacements \
-    winehq-devel \
-    fish \
-    audacity \
-    chromium \
-    dnf-plugin-system-upgrade -y
-
-# Enable RPM Fusion repositories
-sudo dnf groupupdate core
-sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
-sudo dnf groupupdate sound-and-video -y
-
-# Install MSCore fonts
-rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
-
-# Enable better_fonts repository
-sudo dnf copr enable dawid/better_fonts -y
-
-# Enable SSD trimmer
-sudo systemctl enable --now fstrim.timer
-
-# Update the rescue image
-grub2-mkconfig -o /boot/grub2/grub.cfg
-
-# Remove unnecessary packages
-sudo dnf remove -y abrt*
-
-# Ending messages and heads-up
-echo '
-
-
-
-'
-
-echo 'The tweaks are done! Thanks for using my project! NOTICE: Please read if you have and NVIDIA card: https://rpmfusion.org/Howto/NVIDIA, this script cannot install nvidia drivers for you! Please either use this article (recommended), or use fedy to install the drivers you need!'
-
-echo '
-
-
-
-'
-
-echo 'Follow me on github! https://github.com/rockenman1234'
-
-echo '
-
-
-
-'
+function ask_and_run() {
+    local description="$1"
+    local function_name="$2"
     
-echo 'It is imperative to reboot as soon as possible!!!'
+    echo "------------------------------------------------"
+    read -p "[?] apply tweak: $description? [y/N]: " choice
+    case "$choice" in 
+        [yY][eE][sS]|[yY]) 
+            echo ">> [!] applying: $description..."
+            $function_name
+            ;;
+        *) 
+            echo ">> [...] skipped tweak: $description."
+            ;;
+    esac
+    echo ""
+}
 
-# End of file
+function tweak_dnf() {
+    if ! grep -q "max_parallel_downloads" /etc/dnf/dnf.conf; then
+        echo 'max_parallel_downloads=10' | tee -a /etc/dnf/dnf.conf
+    fi
+    if ! grep -q "fastestmirror" /etc/dnf/dnf.conf; then
+        echo 'fastestmirror=true' | tee -a /etc/dnf/dnf.conf
+    fi
+    echo "[!] speeded up dnf."
+}
+
+function update_system() {
+    dnf update -y
+    dnf upgrade --refresh -y
+    dnf check-update -y
+}
+
+function install_repos() {
+    dnf install -y \
+    https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    
+    dnf groupupdate core -y
+    
+    dnf install dnf-plugins-core -y
+    dnf copr enable dawid/better_fonts -y
+}
+
+echo ""
+echo "starting configuration wizard..."
+
+ask_and_run "edit dnf configuration" tweak_dnf
+
+ask_and_run "install RPM Fusion & Better Fonts repositories" install_repos
+
+ask_and_run "apply system tweaks" system_tweaks
+
+echo '
+The tweaks are done! Thanks for using my project! 
+NOTICE: If you have an NVIDIA card, please visit https://rpmfusion.org/Howto/NVIDIA manually.
+'
+echo 'Follow owner of Fedora Tweaks on github! https://github.com/rockenman1234'
+echo 'ftwsl # explore the newer features'
+echo 'It is imperative to reboot as soon as possible!!!'
